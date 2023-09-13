b5639e7945a1b77e57ef0a637785a4ad7199cd3f
5546230 <n.g.j.schotmeijer@student.tudelft.nl>
2023-09-13 13:44:58 +0200

    add feed and profiles

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # Create your views here.
@@ -76,10 +76,15 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.filter(
+        Q(room__topic__name__icontains=q) | Q(room__name__icontains=q)
+    )
+
     context = {
         "rooms": rooms,
         "topics": topics,
         "room_count": room_count,
+        "room_messages": room_messages,
     }
     return render(request, "base/home.html", context)
 
@@ -87,8 +92,31 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     comments = room.message_set.all().order_by("-created")
-    context = {"room": room, "comments": comments}
+    participants = room.participants.all()
+
+    if request.method == "POST":
+        message = request.POST.get("body")
+        if message:
+            Message.objects.create(user=request.user, room=room, body=message)
+            room.participants.add(request.user)
+            return redirect("room", pk=room.id)
+    context = {"room": room, "comments": comments, "participants": participants}
     return render(request, "base/room.html", context)
+
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+
+    context = {
+        "user": user,
+        "rooms": rooms,
+        "room_messages": room_messages,
+        "topics": topics,
+    }
+    return render(request, "base/profile.html", context)
 
 
 @login_required(login_url="login")
@@ -129,3 +157,16 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect("home")
     return render(request, "base/delete.html", {"obj": room})
+
+
+@login_required(login_url="login")
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse("You are not allowed here!")
+
+    if request.method == "POST":
+        message.delete()
+        return redirect("home")
+    return render(request, "base/delete.html", {"obj": message})
